c5ed0ea6756bf60f239670a44e0658a81a8231ef
kimyeongeuk <tklr0731@naver.com>
2024-11-22 17:36:13 +0900

    김영욱
    채팅방 코드

--- src/main/java/com/cl/cruella/handler/ChatEchoHandler.java
+++ src/main/java/com/cl/cruella/handler/ChatEchoHandler.java
@@ -1,8 +1,11 @@
 package com.cl.cruella.handler;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.json.JSONObject;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
@@ -10,17 +13,20 @@ import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import com.cl.cruella.service.ChatServiceImpl;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class ChatEchoHandler extends TextWebSocketHandler{
 	
 	
 	// 웹소켓 세션 객체(클라이언트)들을 저장하는 리스트
-	private List<WebSocketSession> sessionList = new ArrayList<>();
-	
+	private Map<String,Object> map = new HashMap<>();
+	private final ChatServiceImpl chatServiceImpl;
 	
 	/**
 	 * 1) afterConnectionEstablished : 웹소켓에 클라이언트가 연결되었을 때 처리할 내용 정의
@@ -37,7 +43,7 @@ public class ChatEchoHandler extends TextWebSocketHandler{
 		log.debug("현재 채팅방에 참가한 로그인한 회원 : {}",session.getAttributes().get("loginUser"));
 		*/
 		
-		sessionList.add(session);
+		map.put("session", session);
 		
 		//for(WebSocketSession sss : sessionList) {
 			//String msg = "entry|"+ ((MemberDto)session.getAttributes().get("loginUser")).getUserId()+"님이 입장하였습니다.";
@@ -59,13 +65,46 @@ public class ChatEchoHandler extends TextWebSocketHandler{
 		log.debug("WebSocketSession 객체: {}",session);
 		log.debug("WebsocketMessage 객체 : {}",message);
 		log.debug("메세지 내용 : {}", message.getPayload());
+	    
 		
+	    String payload = (String) message.getPayload();
+	    JSONObject jsonObject = new JSONObject(payload); 
+	    String memNo = jsonObject.getString("memNo");
+	    String inviteNo = jsonObject.getString("inviteNo");
+	    String inviteName = jsonObject.getString("inviteName");
+	    
+	    
+	    log.debug("memNo 객체 : {}",memNo);
+	    log.debug("inviteNo 객체 : {}",inviteNo);
+	    log.debug("inviteName 객체 : {}",inviteName);
+	    
+	    
+//	    	Map<String,Object> map = new HashMap<>();
+//		
+//	  		List<String> list = new ArrayList<>();
+//	  		
+//	  		list.add(memNo);
+//	  		list.add(inviteNo);
+//	  		
+//	  		map.put("memNo", memNo);
+//	  		map.put("inviteNo", inviteNo);
+//	  		map.put("inviteName",inviteName);
+//	  		map.put("list", list);
+//	  		
+//	  		String result = chatServiceImpl.checkChatList(list);
+//	  		
+//	  		System.out.println(result);
+//	  		if(result == null) { // 널일경우 채팅방 생성
+//	  			result = String.valueOf(chatServiceImpl.startChat(map)); // 값은 1로담김
+//	  		}
+	  		
+	    
+	    
+	    
 		// 현재 해당 웹소켓에 연결되어있는 모든 클라이언트들(작성자본인포함)에게 현재 들어온 메세지 재발송
-		for(WebSocketSession sss : sessionList) {
-			 //메세지유형(chat/entry/exit)|채팅방에띄워주고자하는메세지내용|발신자아이디
-			String msg = "안녕!";
-			sss.sendMessage(new TextMessage(msg)); // room.jsp에서 onMessage 함수가 자동 실행
-		}
+		
+			System.out.println(map.get("session"));
+		
 		
 		
 		// 특정 회원과 채팅방을 만든다거나 해당 회원과 나눈 채팅내역을 보존한다거나 하려면
@@ -87,7 +126,7 @@ public class ChatEchoHandler extends TextWebSocketHandler{
 		log.debug("현재 채팅방에서 나간 회원 : {}",session.getAttributes().get("loginUser"));
 		*/
 		
-		// sessionList.remove(session);
+		 sessionList.remove(session);
 		
 		//for(WebSocketSession sss : sessionList) {
 			//String msg = "exit|"+ ((MemberDto)session.getAttributes().get("loginUser")).getUserId()+"님이 퇴장하였습니다.";
